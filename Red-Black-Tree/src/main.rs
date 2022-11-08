7e3a927f42a66ed85e82215482e2b653bc6107bb
Yuxin Yue <yyue1@ualberta.ca>
2022-11-08 14:38:48 -0700

    add interface

--- Red-Black-Tree/src/main.rs
+++ Red-Black-Tree/src/main.rs
@@ -1,6 +1,7 @@
 use std::cell::RefCell;
 use std::rc::Rc;
-#[derive(Clone, Debug, PartialEq)] enum NodeColor {
+#[derive(Clone, Debug, PartialEq)]
+enum NodeColor {
     Red,
     Black, 
 }
@@ -9,17 +10,19 @@ type RedBlackTree= Option<Tree>;
 struct TreeNode<T> {
     pub color: NodeColor,
     pub key: T,
-    pub parent: RedBlackTree, left: RedBlackTree, right: RedBlackTree,
+    pub parent: RedBlackTree, 
+    left: RedBlackTree, 
+    right: RedBlackTree,
 }
 
-impl RedBlackTree{
-    fn insert(){}
-    fn delete(){}
-    fn count_leaves(){}
-    fn height(){}
-    fn in_order_traverse(){}
-    fn is_empty(){}
-    fn print(){}
+impl<T> RedBlackTree{
+    pub fn insert(&mut self, value: T){}
+    pub fn delete(&mut self, value: T){}
+    pub fn count_leaves(&self)->u32{}
+    pub fn height(&self)->u32{}
+    pub fn in_order_traverse(&self){}
+    pub fn is_empty(&self)->bool{}
+    pub fn print(&self)->String{}
 }
 
 fn main() {
