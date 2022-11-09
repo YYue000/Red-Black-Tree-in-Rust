8b2923cdf6bbb807f633a69a6ad44f26ef5012b8
Yuxin Yue <yyue1@ualberta.ca>
2022-11-08 20:47:27 -0700

    update struct interface

--- Red-Black-Tree/src/main.rs
+++ Red-Black-Tree/src/main.rs
@@ -6,22 +6,21 @@ enum NodeColor {
     Black, 
 }
 
+type TreeRoot = Option<Rc<RefCell<TreeNode<T>>>>
 struct TreeNode<T> {
     pub color: NodeColor,
     pub key: T,
-    pub parent: RedBlackTree<T>, 
-    left: RedBlackTree<T>, 
-    right: RedBlackTree<T>
+    pub parent: TreeRoot, 
+    left: TreeRoot, 
+    right: TreeRoot
 }
 struct RedBlackTree<T>{
-    root: Option<Rc<RefCell<TreeNode<T>>>>,
+    root: TreeRoot
 }
 
 impl<T> RedBlackTree<T>{
     pub fn insert(&mut self, value: T){}
-    pub fn delete(&mut self, value: T)->Option<T>{
-
-    }
+    pub fn delete(&mut self, value: T)->Option<T>{}
     pub fn count_leaves(&self)->u32{}
     pub fn height(&self)->u32{}
     pub fn in_order_traverse(&self){}
@@ -32,23 +31,6 @@ impl<T> RedBlackTree<T>{
 impl<T: std::cmp::PartialOrd> TreeNode <T>{
     pub fn insert(&mut self, value: T){}
     pub fn delete(&mut self, value: T)->Option<T>{
-        match value{
-            v if v < self.key=>{
-                match self.left.root{
-                    None=>{return None;}
-                    Some(t)=>{return t.borrow_mut().delete(value);}
-                }
-            },
-            v if v > self.key=>{
-                match self.right.root{
-                    None=>{return None;}
-                    Some(t)=>{return t.borrow_mut().delete(value);}
-                }
-            },
-            _=>{}
-        };
-
-        None
     }
 }
 fn main() {
