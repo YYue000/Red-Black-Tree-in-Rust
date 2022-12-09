8102b370a719582da91d24db8c7cc0a6f9c3ec01
R1ckjcx <rickjcx@Gmail.com>
2022-12-09 15:52:22 -0700

    update bench result

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -15,9 +15,9 @@ fn bench_insert_test_avl(array:[i32;5],index:usize) {
     }
 }
 
-fn bench_search_test_avl(tree:AVLTree<i32>,tree_size:i32) {
-    for i in 0..tree_size/10 {
-        tree.search(i);
+fn bench_search_test_avl(mut tree:AVLTree<i32>,tree_size:i32) {
+    for i in 0..tree_size {
+        tree.delete(i);
     }
 }
 
@@ -29,24 +29,24 @@ fn bench_insert_test_rbt(array:[i32;5],index:usize) {
     }
 }
 
-fn bench_search_test_rbt(tree:RedBlackTree<i32>,tree_size:i32) {
+fn bench_search_test_rbt(mut tree:RedBlackTree<i32>,tree_size:i32) {
 
-    for i in 0..tree_size/10 {
-        tree.search(i);
+    for i in 0..tree_size {
+        tree.delete(i);
 
     }
 }
 
 fn avl_benchmark_insert(c: &mut Criterion) {
     let array =[10000, 40000, 70000, 100000, 130000];
-    let array_index = 1; // 0 to 4 for different size test, change this manually for 5 tests
+    let array_index = 4; // 0 to 4 for different size test, change this manually for 5 tests
     println!("Avl benchmark insert test with size {}",array[array_index]);
     c.bench_function("Avl_insert_benchmark", |b| b.iter(|| bench_insert_test_avl(black_box(array),array_index)));
 }
 
 fn avl_benchmark_search(c: &mut Criterion) {
     let array =[10000, 40000, 70000, 100000, 130000];
-    let array_index = 1; // 0 to 4 for different size test, change this manually for 5 tests
+    let array_index = 4; // 0 to 4 for different size test, change this manually for 5 tests
     println!("Avl benchmark search test with size {}",array[array_index]);
 
     let mut tree = AVLTree::new();
@@ -60,14 +60,14 @@ fn avl_benchmark_search(c: &mut Criterion) {
 
 fn rbt_benchmark_insert(c: &mut Criterion) {
     let array =[10000, 40000, 70000, 100000, 130000];
-    let array_index = 1; // 0 to 4 for different size test, change this manually for 5 tests
+    let array_index = 4; // 0 to 4 for different size test, change this manually for 5 tests
     println!("RedBlack Tree benchmark insert test with size {}",array[array_index]);
     c.bench_function("rbt_insert_benchmark", |b| b.iter(|| bench_insert_test_rbt(black_box(array),array_index)));
 }
 
 fn rbt_benchmark_search(c: &mut Criterion) {
     let array =[10000, 40000, 70000, 100000, 130000];
-    let array_index = 1; // 0 to 4 for different size test, change this manually for 5 tests
+    let array_index = 4; // 0 to 4 for different size test, change this manually for 5 tests
     println!("RedBlack Tree benchmark search test with size {}",array[array_index]);
 
     let mut tree = RedBlackTree::new();
@@ -79,9 +79,9 @@ fn rbt_benchmark_search(c: &mut Criterion) {
     c.bench_function("rbt_search_benchmark", |b| b.iter(|| bench_search_test_rbt(black_box(tree.clone()),tree_size)));
 }
 
-criterion_group!(benches, avl_benchmark_insert,avl_benchmark_search, rbt_benchmark_insert, rbt_benchmark_search);
-//criterion_group!(benches,avl_benchmark_search,rbt_benchmark_search);
-
+//criterion_group!(benches, avl_benchmark_insert,avl_benchmark_search, rbt_benchmark_insert, rbt_benchmark_search);
+criterion_group!(benches,avl_benchmark_search,rbt_benchmark_search);
+//criterion_group!(benches, avl_benchmark_insert, rbt_benchmark_insert);
 criterion_main!(benches);
 
 
